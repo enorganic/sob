a429c8b1cfb0bc419dff9e5a891e2b269e5d1189
David Belais <davebelais@gmail.com>
2019-07-31 12:04:38 -0700

    *

--- scripts/clean.py
+++ scripts/clean.py
@@ -5,28 +5,26 @@
 
 import shutil
 import os
-
 from subprocess import getstatusoutput
 
+current_directory = os.path.abspath(os.curdir)
+# Make the parent directory of this file the current directory
+os.chdir(os.path.dirname(__file__))
 package = __file__.split('/')[-3]
-
 for file_or_directory in (
     '../dist', '../build', '../%s.egg-info' % package,
     '../.tox', '../.cache', '../venv',
     '../.pytest_cache', '../dist'
 ):
     if os.path.exists(file_or_directory):
-
         command = (
             'git rm -r -f --cached --ignore-unmatch "%s"' % file_or_directory
         )
-
         print(command)
-
         status, output = getstatusoutput(command)
-
         if status != 0:
             raise OSError(output)
-
         if os.path.exists(file_or_directory):
             shutil.rmtree(file_or_directory)
+# Change back to the original directory
+os.chdir(current_directory)
--- scripts/distribute.py
+++ scripts/distribute.py
@@ -1,30 +1,26 @@
-# !python3.7
+# !/usr/bin/python3.7
 import os
 from subprocess import getstatusoutput
-
 from setuptools_setup_versions import install_requires
 
-# Change to the package directory
-os.chdir('../')
-
 # Update `setup.py` to require currently installed versions of all packages
-install_requires.update_versions(operator='>=')
-
+install_requires.update_versions('../', operator='>=')
 # Build
 status, output = getstatusoutput(
-    'py -3.7 setup.py sdist bdist_wheel upload clean --all'
+    'py -3.7 ../setup.py sdist bdist_wheel upload clean --all'
     if os.name == 'nt' else
-    'python3.7 setup.py sdist bdist_wheel upload clean --all'
+    'python3.7 ../setup.py sdist bdist_wheel upload clean --all'
 )
-
+# Create an error if a non-zero exit status is encountered
 error = None
-
 if status:
     error = OSError(output)
 else:
     print(output)
-
-exec(open('./scripts/clean.py').read())
-
+exec(
+    open('./scripts/clean.py').read(),
+    {'__file__': os.path.abspath('./scripts/clean.py')}
+)
+# Raise the previously created error, if there was one
 if error:
     raise error
